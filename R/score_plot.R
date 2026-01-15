9ffa1b6e61d2e74a236ce42c6b93a7f4808a9a07
Huong Tran <quynhhuong5335@gmail.com>
2026-01-14 22:12:38 -0500

    update score plot

--- R/score_plot.R
+++ R/score_plot.R
@@ -89,7 +89,7 @@ score_plot1D <- function(x, P = NULL,
     lower <- c(lower, loweri)
   }
   df <- data.frame(x = x, a = a, f = f, upper = upper, lower = lower)
-  ind <- which(df$a <= df$lower | df$a >= df$upper)
+  # ind <- which(df$a <= df$lower | df$a >= df$upper)
   # 20250113 - HT - update outliers based on symmetric distance
   df$dev <- df$a - df$f
   df$crit <- 2*sqrt(bi)
@@ -98,8 +98,8 @@ score_plot1D <- function(x, P = NULL,
   out <- index[ind]
   #####
   # df$Type <- (df$a <= df$lower | df$a >= df$upper)
-  df$Type <- as.factor(ifelse(df$a < lower | df$a > upper,
-                              "Outlier", "Non outlier"))
+  # df$Type <- as.factor(ifelse(df$a < lower | df$a > upper,
+  #                             "Outlier", "Non outlier"))
   df$Type <- factor(df$Type,
                     levels = c(T, F),
                     labels = c("Outlier", "Non-outlier"))
